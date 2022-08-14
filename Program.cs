61708e6bcb7955ec350f0f91a01b96e5ca58909b
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-08-14 13:41:10 +0300

    Решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -22,3 +22,24 @@ if (number1 < 100)
 {
     Console.WriteLine("третьей цифры нет");
 }
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число от 1 до 7");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA > 5)
+{
+    Console.WriteLine("данный день - выходной!");
+}
+
+if (numberA <= 5)
+{
+    Console.WriteLine("данный день является будничным");
+}    
+
+
+
+
+
+
+
