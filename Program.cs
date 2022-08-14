a3528b73c92136b4d3333348017c25efd9d24d4a
Rodion Gibadullin <rodion-stalker@mail.ru>
2022-08-14 13:22:12 +0300

    решение второй задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,19 @@ int number = Convert.ToInt32(Console.ReadLine());
 if(number >= 100 && number <= 999);
 int S = (number%100)/10;
 Console.WriteLine("Вторая цифра данного числа: " + S);
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 >= 100 && number1 <= 999)
+{
+    int A = number1%10;
+    Console.WriteLine("Третья цифра данного числа: " + A);
+}
+
+if (number1 < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+}
